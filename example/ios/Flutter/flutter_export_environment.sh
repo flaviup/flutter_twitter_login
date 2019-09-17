78a949d648f75619795bf85aff4d7d371ea27754
Flaviu Pasca <flaviup@gmail.com>
2019-09-17 14:54:16 +0300

    iOS deployment target set to 9.0.

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/flaviupasca/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/flaviupasca/Projects/FlutterProjects/flutter_twitter_login/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/flaviupasca/flutter/bin/cache/artifacts/engine/ios-release"
